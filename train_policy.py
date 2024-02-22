6a14b8506d6edd67ac3dcf48c8b070edc5ab46f8
JornEirikBetten <jorneirikbetten1@gmail.com>
2024-02-22 13:52:57 +0100

    asd

--- train_policy.py
+++ train_policy.py
@@ -17,6 +17,9 @@ from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.logger import configure
 import src as src 
 
+# Set environment and learning algorithm
+environment = "MinAtar/Freeway-v1" # "MinAtar/Seaquest-v1"
+algorithm = "dqn"
 policy_kwargs = dict(
     features_extractor_class=src.FreewayCNN,
     features_extractor_kwargs=dict(features_dim=128),
@@ -27,24 +30,39 @@ freeway = "freeway"
 dqn_algo = "dqn"
 ppo_algo = "ppo"
 
-policy_names =["beta", "gamma", "delta", "epsilon"]
+if environment == "MinAtar/Freeway-v1": 
+    env_name = freeway 
+if environment == "MinAtar/Seaquest-v1": 
+    env_name = seaquest 
+    
+if algorithm == "dqn": 
+    algo = dqn_algo
+if algorithm == "ppo": 
+    algo = ppo_algo
 
-eval_env = gym.make("MinAtar/Freeway-v1")
+
+
+policy_names =["alpha", "beta", "gamma"]
+
+
+
+eval_env = gym.make(environment)
+
 path = os.getcwd()    
-log_path = path + "/training_runs/" + freeway + "/" + ppo_algo + "/"
+log_path = path + "/training_runs/" + env_name + "/" + algo + "/"
 
-total_timesteps = 5_000_000
+total_timesteps = 10_000_000
 for i, policy_name in enumerate(policy_names): 
     save_path = log_path + policy_name + "/"
     eval_callback = EvalCallback(Monitor(eval_env), best_model_save_path=save_path,
                              log_path=save_path, eval_freq=100_000,
                              deterministic=True, render=False)
-    env = gym.make("MinAtar/Freeway-v1")
+    env = gym.make(environment)
     env = Monitor(env, filename=save_path)
     env_checker.check_env(env)
     sb3_common.env_checker.check_env(env)
     in_channels, num_actions = env.observation_space.shape[2], env.action_space.n
-    model = PPO("CnnPolicy", 
+    """ model = PPO("CnnPolicy", 
                 env, 
                 n_steps=2048, 
                 policy_kwargs=policy_kwargs, 
@@ -52,18 +70,18 @@ for i, policy_name in enumerate(policy_names):
                 verbose=1, 
                 seed = 1231, 
                 torch_seed = 1123 + 123451*(i+1), 
-                device = "auto")  
-    """ model = dqn.DQN("CnnPolicy", 
+                device = "auto")   """
+    model = dqn.DQN("CnnPolicy", 
                     env, 
                     buffer_size=200_000, 
                     policy_kwargs=policy_kwargs, 
                     target_update_interval=50_000, 
                     verbose=1,
                     seed = 1231, 
-                    torch_seed = 1123 + 123451*(i+0))   """
+                    torch_seed = 1123 + 123451*(i+0)) 
     model.learn(total_timesteps=total_timesteps, callback=eval_callback, progress_bar=True)
     print("Training completed.")
-    model.save(log_path + policy_name + "_" + freeway + "_" + ppo_algo)
+    model.save(log_path + policy_name + "_" + env_name + "_" + algo)
 
     del model # remove to demonstrate saving and loading
 
--- train_rl_agents.sh
+++ train_rl_agents.sh
@@ -9,4 +9,6 @@
 #SBATCH --gres=gpu:1
 
 echo "Starting job at time:" && date +%Y-%m-%d_%H:%M:%S
-export LD_LIBRARY_PATH=/cm/shared/apps/cuda12.1/toolkit/12.1.1/targets/x86_64-linux/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+echo $CUDA_VISIBLE_DEVICES
+
+srun -n 1 python train_policy.py 
